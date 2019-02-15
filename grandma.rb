05e9a66c5ffa1d05eec23304f9c6b09de8d972dc
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-15 23:49:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return  "I LOVE YOU TOO PUMPKIN"
+    return  "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
 else
